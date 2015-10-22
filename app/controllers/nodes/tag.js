924565c29589b128077e54a48e5ecf420cf31b6d
asanfilippo7 <asanfilippo777@gmail.com>
2015-10-22 11:19:11 -0400

    Refined sorting to use model

--- app/controllers/nodes/tag.js
+++ app/controllers/nodes/tag.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    
+    tagID: function() {
+        var href = window.location.href;
+        var theID = href.substr(href.lastIndexOf('/') + 1).replace("%20"," ");
+        return theID;
+    }.property('model.tagID')
 });
