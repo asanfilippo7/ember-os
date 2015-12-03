15f18b0c8d592cd0fd62f0feaae10fb86631ad94
asanfilippo7 <asanfilippo777@gmail.com>
2015-12-03 11:46:31 -0500

    Revert "Minor change to users template: hide nodes if none."
    This reverts commit b79c71dbdfad68b91ffd9e005edcdc62ae7c9d2b.
    
    Undo because something went wrong..

--- app/routes/users/user.js
+++ app/routes/users/user.js
@@ -4,13 +4,5 @@ export default Ember.Route.extend({
     model: function(params) {
         console.log('user route running');
         return this.store.findRecord('user', params.user_id);
-    },
-//    actions: {
-//        reName: function(newName) {
-//           var changed = this.get('currentModel');
-//            changed.set('givenName', newName);
-//            console.log('new given name', changed.get('givenName'));
-//            changed.save();
-//        }
-//    }
+    }
 });
