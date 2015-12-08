258906472306be76106f152421f2b563a931fbf9
asanfilippo7 <asanfilippo777@gmail.com>
2015-12-08 13:51:05 -0500

    Progress on loading full dataset for paginated response. See recursive method in contributor serializer.

--- app/serializers/contributor.js
+++ app/serializers/contributor.js
@@ -6,17 +6,49 @@ export default DS.RESTSerializer.extend({
     },
     
     normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+        
+        function getNext(theArrayPassed, theNewArray) {
+            console.log('get next running');
+            if(theArrayPassed.links.next) {
+                var theOtherJSON = $.getJSON(theArrayPassed.links.next, function(returnedArray) {
+                    returnedArray.data.map(function(arrayObj) {
+                        theNewArray.push(arrayObj);
+                    });
+                    getNext(returnedArray, theNewArray);
+                });
+            }
+            else {
+                console.log('no more next!')
+                var arr = [];
+                theNewArray.map(function(record) {
+                    record.type = primaryModelClass.modelName;
+                    record.links.user = record.embeds.users.data.links.self;
+                    arr.push(record);
+                });
+            }
+            console.log('about to return ', arr);
+            return arr;
+        }
+        
+        console.log('normalizing contributor response');
         var normalizedRecords = [];
-    
+        
+//        var theData = payload.data;
+//        var nr = [];
+//        console.log(getNext(payload, theData));
+        
+        
         payload.data.map(function(record) {
             record.type = primaryModelClass.modelName;
             record.links.user = record.embeds.users.data.links.self;
             normalizedRecords.push(record);
         });
+        console.log('normalized records', normalizedRecords);
+        
         var obj = {};
         obj[primaryModelClass.modelName] = normalizedRecords;
         obj.meta = payload.links.meta;
-        
+                                         
         return this._super(store, primaryModelClass, obj, id, requestType);
     }
 });
